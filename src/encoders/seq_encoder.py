20ab41ca16ade9fdd7dbcdb7cf650dce2250689f
Maximilian Schall <maxscha@mail.de>
2020-01-31 11:47:49 +0100

    Adds start end token

--- src/encoders/seq_encoder.py
+++ src/encoders/seq_encoder.py
@@ -17,18 +17,18 @@ from .encoder import Encoder, QueryType
 class SeqEncoder(Encoder):
     @classmethod
     def get_default_hyperparameters(cls) -> Dict[str, Any]:
-        encoder_hypers = { 'token_vocab_size': 50000,
-                           'token_vocab_count_threshold': 5,
-                           'token_embedding_size': 128,
+        encoder_hypers = {'token_vocab_size': 50000,
+                          'token_vocab_count_threshold': 5,
+                          'token_embedding_size': 128,
 
-                           'use_subtokens': False,
-                           'mark_subtoken_end': False,
+                          'use_subtokens': False,
+                          'mark_subtoken_end': False,
 
-                           'max_num_tokens': 200,
+                          'max_num_tokens': 200,
 
-                           'use_bpe': True,
-                           'pct_bpe': 0.5
-                         }
+                          'use_bpe': True,
+                          'pct_bpe': 0.5
+                          }
         hypers = super().get_default_hyperparameters()
         hypers.update(encoder_hypers)
         return hypers
@@ -38,9 +38,11 @@ class SeqEncoder(Encoder):
     def __init__(self, label: str, hyperparameters: Dict[str, Any], metadata: Dict[str, Any]):
         super().__init__(label, hyperparameters, metadata)
         if hyperparameters['%s_use_bpe' % label]:
-            assert not hyperparameters['%s_use_subtokens' % label], 'Subtokens cannot be used along with BPE.'
+            assert not hyperparameters['%s_use_subtokens' %
+                                       label], 'Subtokens cannot be used along with BPE.'
         elif hyperparameters['%s_use_subtokens' % label]:
-            assert not hyperparameters['%s_use_bpe' % label], 'Subtokens cannot be used along with BPE.'
+            assert not hyperparameters['%s_use_bpe' %
+                                       label], 'Subtokens cannot be used along with BPE.'
 
     def _make_placeholders(self):
         """
@@ -93,28 +95,32 @@ class SeqEncoder(Encoder):
 
     @classmethod
     def load_metadata_from_sample(cls, data_to_load: Iterable[str], raw_metadata: Dict[str, Any],
-                                  use_subtokens: bool=False, mark_subtoken_end: bool=False) -> None:
+                                  use_subtokens: bool = False, mark_subtoken_end: bool = False) -> None:
         if use_subtokens:
-            data_to_load = cls._to_subtoken_stream(data_to_load, mark_subtoken_end=mark_subtoken_end)
+            data_to_load = cls._to_subtoken_stream(
+                data_to_load, mark_subtoken_end=mark_subtoken_end)
 
         raw_metadata['token_counter'].update(cls.make_to_ngram(data_to_load))
 
     @classmethod
     def finalise_metadata(cls, encoder_label: str, hyperparameters: Dict[str, Any], raw_metadata_list: List[Dict[str, Any]]) -> Dict[str, Any]:
-        final_metadata = super().finalise_metadata(encoder_label, hyperparameters, raw_metadata_list)
+        final_metadata = super().finalise_metadata(
+            encoder_label, hyperparameters, raw_metadata_list)
         merged_token_counter = Counter()
-        for raw_metadata in raw_metadata_list:            
+        for raw_metadata in raw_metadata_list:
             merged_token_counter += raw_metadata['token_counter']
 
         print(len(merged_token_counter))
         if hyperparameters['%s_use_bpe' % encoder_label]:
             token_vocabulary = BpeVocabulary(vocab_size=hyperparameters['%s_token_vocab_size' % encoder_label],
-                                             pct_bpe=hyperparameters['%s_pct_bpe' % encoder_label]
+                                             pct_bpe=hyperparameters['%s_pct_bpe' %
+                                                                     encoder_label]
                                              )
             token_vocabulary.fit(merged_token_counter)
         else:
             token_vocabulary = Vocabulary.create_vocabulary(tokens=merged_token_counter,
-                                                            max_size=hyperparameters['%s_token_vocab_size' % encoder_label],
+                                                            max_size=hyperparameters['%s_token_vocab_size' %
+                                                                                     encoder_label],
                                                             count_threshold=hyperparameters['%s_token_vocab_count_threshold' % encoder_label])
 
         print(token_vocabulary)
@@ -126,8 +132,10 @@ class SeqEncoder(Encoder):
     @classmethod
     def make_to_ngram(cls, data):
         result = []
+        data.insert(0, "<start>")
+        data.append("<end>")
         for i in range(len(data) - 1):
-            result.append(data[i] + "_" +data[i+1])
+            result.append(data[i] + "_" + data[i+1])
         return result
 
     @classmethod
@@ -148,14 +156,16 @@ class SeqEncoder(Encoder):
         data_to_load = cls.make_to_ngram(data_to_load)
 
         # Save the two versions of the code and query:
-        data_holder = {QueryType.DOCSTRING.value: data_to_load, QueryType.FUNCTION_NAME.value: None}
+        data_holder = {QueryType.DOCSTRING.value: data_to_load,
+                       QueryType.FUNCTION_NAME.value: None}
         # Skip samples where the function name is very short, because it probably has too little information
         # to be a good search query.
         if not is_test and hyperparameters['fraction_using_func_name'] > 0. and function_name and \
                 len(function_name) >= hyperparameters['min_len_func_name_for_query']:
             if encoder_label == 'query':
                 # Set the query tokens to the function name, broken up into its sub-tokens:
-                data_holder[QueryType.FUNCTION_NAME.value] = split_identifier_into_parts(function_name)
+                data_holder[QueryType.FUNCTION_NAME.value] = split_identifier_into_parts(
+                    function_name)
             elif encoder_label == 'code':
                 # In the code, replace the function name with the out-of-vocab token everywhere it appears:
                 data_holder[QueryType.FUNCTION_NAME.value] = [Vocabulary.get_unk() if token == function_name else token
@@ -179,7 +189,8 @@ class SeqEncoder(Encoder):
             # unique-ish
             result_holder[f'{encoder_label}_tokens_{key}'] = tokens
             result_holder[f'{encoder_label}_tokens_mask_{key}'] = tokens_mask
-            result_holder[f'{encoder_label}_tokens_length_{key}'] = int(np.sum(tokens_mask))
+            result_holder[f'{encoder_label}_tokens_length_{key}'] = int(
+                np.sum(tokens_mask))
 
         if result_holder[f'{encoder_label}_tokens_mask_{QueryType.DOCSTRING.value}'] is None or \
                 int(np.sum(result_holder[f'{encoder_label}_tokens_mask_{QueryType.DOCSTRING.value}'])) == 0:
@@ -187,7 +198,7 @@ class SeqEncoder(Encoder):
 
         return True
 
-    def extend_minibatch_by_sample(self, batch_data: Dict[str, Any], sample: Dict[str, Any], is_train: bool=False,
+    def extend_minibatch_by_sample(self, batch_data: Dict[str, Any], sample: Dict[str, Any], is_train: bool = False,
                                    query_type: QueryType = QueryType.DOCSTRING.value) -> bool:
         """
         Implements various forms of data augmentation.
@@ -206,30 +217,41 @@ class SeqEncoder(Encoder):
             total_length = len(current_sample['tokens'])
             length_without_padding = current_sample['tokens_lengths']
             # Generate a list of places in which to insert tokens:
-            insert_indices = np.array([random.uniform(0., 1.) for _ in range(length_without_padding)])  # don't allow insertions in the padding
-            insert_indices = insert_indices < self.hyperparameters['query_random_token_frequency']  # insert at the correct frequency
+            insert_indices = np.array([random.uniform(0., 1.) for _ in range(
+                length_without_padding)])  # don't allow insertions in the padding
+            # insert at the correct frequency
+            insert_indices = insert_indices < self.hyperparameters['query_random_token_frequency']
             insert_indices = np.flatnonzero(insert_indices)
             if len(insert_indices) > 0:
                 # Generate the random tokens to add:
                 tokens_to_add = [random.randrange(0, len(self.metadata['common_tokens']))
                                  for _ in range(len(insert_indices))]  # select one of the most common tokens for each location
-                tokens_to_add = [self.metadata['common_tokens'][token][0] for token in tokens_to_add]  # get the word corresponding to the token we're adding
-                tokens_to_add = [self.metadata['token_vocab'].get_id_or_unk(token) for token in tokens_to_add]  # get the index within the vocab of the token we're adding
+                # get the word corresponding to the token we're adding
+                tokens_to_add = [self.metadata['common_tokens']
+                                 [token][0] for token in tokens_to_add]
+                # get the index within the vocab of the token we're adding
+                tokens_to_add = [self.metadata['token_vocab'].get_id_or_unk(
+                    token) for token in tokens_to_add]
                 # Efficiently insert the added tokens, leaving the total length the same:
                 to_insert = 0
                 output_query = np.zeros(total_length, dtype=int)
-                for idx in range(min(length_without_padding, total_length - len(insert_indices))):  # iterate only through the beginning of the array where changes are being made
+                # iterate only through the beginning of the array where changes are being made
+                for idx in range(min(length_without_padding, total_length - len(insert_indices))):
                     if to_insert < len(insert_indices) and idx == insert_indices[to_insert]:
-                        output_query[idx + to_insert] = tokens_to_add[to_insert]
+                        output_query[idx +
+                                     to_insert] = tokens_to_add[to_insert]
                         to_insert += 1
-                    output_query[idx + to_insert] = current_sample['tokens'][idx]
+                    output_query[idx +
+                                 to_insert] = current_sample['tokens'][idx]
                 current_sample['tokens'] = output_query
                 # Add the needed number of non-padding values to the mask:
-                current_sample['tokens_mask'][length_without_padding:length_without_padding + len(tokens_to_add)] = 1.
+                current_sample['tokens_mask'][length_without_padding:length_without_padding + len(
+                    tokens_to_add)] = 1.
                 current_sample['tokens_lengths'] += len(tokens_to_add)
 
         # Add the current sample to the minibatch:
-        [batch_data[key].append(current_sample[key]) for key in current_sample.keys() if key in batch_data.keys()]
+        [batch_data[key].append(current_sample[key])
+         for key in current_sample.keys() if key in batch_data.keys()]
 
         return False
 
